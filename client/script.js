f70d27547a5f13b34ace26436ec661b491d9328d
NylasDev <sergiuvataman.dev@gmail.com>
2023-04-03 22:10:25 +0300

    done for the day with this, final commit

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server -> bot response
-  const response = await fetch('https://machinespiritlisten.theviking.dev/', {
+  const response = await fetch('http://localhost:5000', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
